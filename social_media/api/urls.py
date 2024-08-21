673ad522eab40d02bb03e5fae8b25e63d9c5ef05
dark <milad73n@gmail.com>
2024-08-21 22:57:36 +0330

    add user app2

--- social_media/api/urls.py
+++ social_media/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-        path('blog/', include(('social_media.blog.urls', 'blog')))
+        path('blog/', include(('social_media.blog.urls', 'blog'))),
+        path('user/', include(('social_media.users.urls', 'user'))),
+        path('auth/', include(('social_media.authentication.urls', 'auth')))
 ]
--- social_media/users/validators.py
+++ social_media/users/validators.py
@@ -4,17 +4,24 @@ from django.core.exceptions import ValidationError
 import re
 
 
+# def number_validator(password):
+#     regex = re.compile('[0-9]')
+#     if regex.search(password) == None:
+#         raise ValidationError(
+#                 _("password must include number"),
+#                 code="password_must_include_number"
+#                 )
 def number_validator(password):
-    regex = re.compile('[0-9]')
-    if regex.search(password) == None:
+    regex = re.compile(r'\d')
+    if not regex.search(password):
         raise ValidationError(
-                _("password must include number"),
-                code="password_must_include_number"
-                )
+            _("The password must include at least one number."),
+            code="password_must_include_number"
+        )
 
 def letter_validator(password):
     regex = re.compile('[a-zA-Z]')
-    if regex.search(password) == None:
+    if not regex.search(password):
         raise ValidationError(
                 _("password must include letter"),
                 code="password_must_include_letter"
